d125ebff1b1666e77b4cc0d6f2d6eb4d130be415
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2020-10-24 21:49:31 +0200

    ADD: Active menu InterestionObserver

--- scripts/script.js
+++ scripts/script.js
@@ -136,3 +136,33 @@ const sectionOneObserver = new IntersectionObserver(
 // For Animated Header when scroll
 sectionOneObserver.observe(header);
 
+
+// ACTIVE MENU SECTION
+// simple function to use for callback in the intersection observer
+const changeNav = (entries, observer) => {
+    entries.forEach((entry) => {
+        // verify the element is intersecting
+        if(entry.isIntersecting && entry.intersectionRatio >= 0.55) {
+			// remove old active class
+            document.querySelector('.active').classList.remove('active');
+            // get id of the intersecting section
+            var id = entry.target.getAttribute('id');
+            // find matching link & add appropriate class
+            var newLink = document.querySelector(`[href="#${id}"]`).classList.add('active');
+        }
+    });
+}
+
+// init the observer
+const options = {
+    threshold: 0.55
+}
+
+const observer = new IntersectionObserver(changeNav, options);
+
+// target the elements to be observed
+const sections = document.querySelectorAll('header, section');
+sections.forEach((section) => {
+    observer.observe(section);
+});
+
